d315ac64409fbd7de83f002d4ea8b5be13437e80
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-08-01 19:50:44 -0400

    reduce LOC , remove unused variables, remove unneeded hooks

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,13 @@
-#include "Geode/loader/Log.hpp"
-#include <Geode/Bindings.hpp>
-#include <Geode/cocos/layers_scenes_transitions_nodes/CCLayer.h>
-#include <cocos2d.h>
-#include <Geode/modify/PlayerObject.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/EndLevelLayer.hpp>
 
 using namespace geode::prelude;
 
-static bool g_isInLevel = false;
-
-class $modify(PlayLayer) {
-    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-        if (!PlayLayer::init(level, useReplay, dontCreateObjects))
-            return false;
-        // Gameplay is starting
-        g_isInLevel = true;
-        return true;
-    }
-    void onExit() {
-        // Gameplay is ending
-        g_isInLevel = false;
-        PlayLayer::onExit();
-    }
-};
-
 class BlackoutLayer : public cocos2d::CCLayerColor {
-    float _duration;
 public:
     static BlackoutLayer* createWithDuration(float duration) {
         cocos2d::ccColor4B color = Mod::get()->getSettingValue<cocos2d::ccColor4B>("color");
         auto layer = new BlackoutLayer();
         if (layer && layer->initWithColor(color)) {
-            layer->_duration = duration;
             layer->scheduleOnce(
                 schedule_selector(BlackoutLayer::onBlackoutFinished),
                 duration
@@ -49,19 +24,16 @@ public:
     }
 };
 
-
 class $modify(PlayerObject) {
-    struct Fields { };
-
     bool pushButton(PlayerButton button) {
         // Always call the original tap logic first
-        PlayerObject::pushButton(button);
+        bool result = PlayerObject::pushButton(button);
 
         // Only active during levels
-        if (!g_isInLevel) return true;
+        if (!PlayLayer::get()) return result;
 
         float dur = Mod::get()->getSettingValue<float>("duration");
-        auto scene = cocos2d::CCDirector::sharedDirector()->getRunningScene();
+        auto scene = CCScene::get();
         if (scene) {
             auto black = BlackoutLayer::createWithDuration(dur);
             if (black) {
@@ -69,6 +41,6 @@ class $modify(PlayerObject) {
                 scene->addChild(black, INT_MAX);
             }
         }
-        return true;
+        return result;
     }
 };
